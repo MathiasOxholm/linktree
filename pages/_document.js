9955bca3f3f0af4ade80e4d2864fd4afefa31020
Mathias Oxholm <mo.micheelsen@gmail.com>
2022-04-19 22:42:13 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Syne:wght@800&display=swap"
           rel="stylesheet"
